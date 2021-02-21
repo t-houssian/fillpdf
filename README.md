2fd471f4e84e3c21fc6c523c7be92420effb55c7
t-houssian <tylerhoussian@gmail.com>
2021-02-20 18:43:42 -0700

    First push v0.0.1 fillpdfs

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fillpdf
-A python library to make filling pdfs much easier
+
+This is a simple package to make filling pdfs much easier.
+
+- Fills pdfs
+- Lists fields in pdf
+- Flattens pdfs (Turns to an image)
+
+- requires manually install of poppler (conda install -c conda-forge poppler)
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+install_requires = [
+    'pdfrw',
+    'pdf2image',
+    'Pillow',
+    'PyPDF2',
+    ]
+
+setup(
+    name='fillpdf',
+    packages=find_packages(exclude=['tests']),
+    version='0.0.1',
+    install_requires=install_requires,
+    description='A Library to fill and flatten pdfs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Tyler Houssian',
+    author_email="tylerhoussian@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    url='https://github.com/t-houssian/fillpdf',
+)
